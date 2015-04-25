9e21972ac9723c9fc76c4795ed9de2a6b4e66c31
Mert Talubas <dmtmanga@gmail.com>
2015-04-25 17:28:30 -0400

    level layout

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelController : MonoBehaviour {
+	
+	public GameObject grassPrefab;
+	//public GameObject bushPrefab;
+
+	// Use this for initialization
+	void Start () {
+		PrepareLevel ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void PrepareLevel() {
+		GameObject currentTile;
+		Vector3 position = new Vector3 ();
+		for (int i = -12; i < 13; i++) {
+			if (i >-2 && i < 2 ) continue;
+			for (int j = -6; j < 7; j++) {
+				position.x = i * 0.64f;
+				position.y = j * 0.64f;
+				currentTile = (GameObject) Instantiate( grassPrefab, position, new Quaternion() );
+			}
+		}
+	}
+}
