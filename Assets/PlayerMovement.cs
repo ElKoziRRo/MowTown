ac6d2cad69f6cb482c938344783febc4cc2cb28e
Mert Talubas <dmtmanga@gmail.com>
2015-04-25 14:06:27 -0400

    project dump

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;	
+	private Rigidbody2D r;
+
+	private Vector3 moveVector;
+	private string x;
+	private string y;
+
+
+
+	// Use this for initialization
+	void Start () {
+		r = GetComponent<Rigidbody2D>();
+		if (name == "P1") {
+			x = "LeftStickX1";
+			y = "LeftStickY1";
+		}else { //P2 
+			x = "LeftStickX2";
+			y = "LeftStickY2";
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveVector.x = Input.GetAxis (x) * speed;
+		moveVector.y = Input.GetAxis (y) * speed * -1;
+		moveVector.z = 0f;
+
+		//transform.position += moveVector * Time.deltaTime;
+
+		r.velocity = moveVector;
+	}
+}
